ddf68c8994c7ab61b405050c6569df66c85e76ca
kjkandrea <kjkandrea@gmail.com>
2022-12-24 15:59:32 +0900

    feat: Screening 생성

--- src/screening-reservation/index.ts
+++ src/screening-reservation/index.ts
@@ -1,1 +1,15 @@
-console.log('hello');
+interface Movie {
+  foo: 'bar';
+}
+
+class Screening {
+  private movie: Movie;
+  private sequence: number; // 순번
+  private whenScreened: Date; // 상영 시작 시간
+
+  constructor(movie: Movie, sequence: number, whenScreened: Date) {
+    this.movie = movie;
+    this.sequence = sequence;
+    this.whenScreened = whenScreened;
+  }
+}
