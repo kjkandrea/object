a7361fbb3e27e9b8e274e5e7a6657f016365c3b4
kjkandrea <kjkandrea@gmail.com>
2022-12-24 16:07:51 +0900

    feat: Screening 메서드 추가

--- src/screening-reservation/index.ts
+++ src/screening-reservation/index.ts
@@ -1,15 +1,27 @@
-interface Movie {
-  foo: 'bar';
+abstract class Movie {
+  public abstract getFee(): number;
 }
 
 class Screening {
   private movie: Movie;
   private sequence: number; // 순번
-  private whenScreened: Date; // 상영 시작 시간
+  private readonly whenScreened: Date; // 상영 시작 시간
 
   constructor(movie: Movie, sequence: number, whenScreened: Date) {
     this.movie = movie;
     this.sequence = sequence;
     this.whenScreened = whenScreened;
+  }
+
+  public getStartTime(): Readonly<Date> {
+    return this.whenScreened;
+  }
+
+  public isSequence(sequence: number): boolean {
+    return this.sequence === sequence;
+  }
+
+  public getMovieFee(): number {
+    return this.movie.getFee();
   }
 }
