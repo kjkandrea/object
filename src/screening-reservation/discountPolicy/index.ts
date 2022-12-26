ce7df153826380fdb92280263eb92174dad5c3f8
kjkandrea <kjkandrea@gmail.com>
2022-12-26 14:23:16 +0900

    feat: PeriodCondition 추가

--- src/screening-reservation/discountPolicy/index.ts
+++ src/screening-reservation/discountPolicy/index.ts
@@ -23,6 +23,7 @@ export abstract class DiscountPolicy {
   protected abstract getDiscountAmount(screening: Screening): Money;
 }
 
+// 조조할인 등 순번할인 조건
 export class SequenceCondition implements DiscountCondition {
   private readonly sequence: number;
 
@@ -32,5 +33,33 @@ export class SequenceCondition implements DiscountCondition {
 
   public isSatisfiedBy(screening: Screening): boolean {
     return screening.isSequence(this.sequence);
+  }
+}
+
+// 상영 시작 시간이 특정한 기간안에 포함되는지 여부
+export class PeriodCondition implements DiscountCondition {
+  private dayOfWeek: ReturnType<Date['getDay']>;
+  private startTime: Date;
+  private endTime: Date;
+
+  constructor(
+    dayOfWeek: ReturnType<Date['getDay']>,
+    startTime: Date,
+    endTime: Date
+  ) {
+    this.dayOfWeek = dayOfWeek;
+    this.startTime = startTime;
+    this.endTime = endTime;
+  }
+
+  /**
+   * @return 상영요일이 dayOfWeek 와 같고 상영 시작 시간이 startTime 과 endTime 사이에 있을 경우
+   */
+  public isSatisfiedBy(screening: Screening): boolean {
+    const matchedWeek = screening.getStartTime().getDay() === this.dayOfWeek;
+    const moreThenStartTime = screening.getStartTime() >= this.startTime;
+    const lessThenEndTime = this.endTime >= screening.getStartTime();
+
+    return matchedWeek && moreThenStartTime && lessThenEndTime;
   }
 }
