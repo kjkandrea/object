c4dfe6192056a9df6956af7a431d7189a661f003
kjkandrea <kjkandrea@gmail.com>
2022-12-31 12:41:38 +0900

    feat: Screening 책임을 수행하는데에 필요한 인스턴스 변수 추가

--- src/screening-reservation/responsibility-driven/Screening.ts
+++ src/screening-reservation/responsibility-driven/Screening.ts
@@ -1,7 +1,12 @@
 import {Reservation} from './Reservation';
 import {Customer} from './Customer';
+import {Movie} from './Movie';
 
 export class Screening {
+  private movie: Movie;
+  private sequence: number;
+  private whenScreened: Date;
+
   public reserve(customer: Customer, audienceCount: number): Reservation {
     return new Reservation();
   }
