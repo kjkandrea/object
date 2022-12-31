fe3464dba9deab29162aa7bd189a1a6b8d65c6fd
kjkandrea <kjkandrea@gmail.com>
2022-12-31 13:40:50 +0900

    feat: 인터페이스로 DiscountCondition 역할을 추상화

--- src/screening-reservation/responsibility-driven/DiscountCondition/index.ts
+++ src/screening-reservation/responsibility-driven/DiscountCondition/index.ts
@@ -1,43 +1,11 @@
 import {Screening} from '../Screening';
 import {DayOfWeek} from '../../../global/datetime/dayOfWeek';
 
-type DiscountConditionType = 'SEQUENCE' | 'PERIOD';
-
-/**
- * 변경에 취약한 클래스
- *
- * 새로운 할인 조건이 추가 될때, 순번 조건 로직 변경 시, 기간 조건 판단 로직 변경 시
- */
-export abstract class DiscountCondition {
-  private readonly type: DiscountConditionType;
-
-  private readonly sequence: number;
-  private readonly dayOfWeek: DayOfWeek;
-  private readonly startTime: Date;
-  private readonly endTime: Date;
-
-  public isSatisfiedBy(screening: Screening): boolean {
-    if (this.type === 'PERIOD') {
-      return this.isSatisfiedByPeriod(screening);
-    }
-
-    return this.isSatisfiedBySequence(screening);
-  }
-
-  private isSatisfiedByPeriod(screening: Screening): boolean {
-    const matchedWeek = screening.getWhenScreened().getDay() === this.dayOfWeek;
-    const moreThenStartTime = screening.getWhenScreened() >= this.startTime;
-    const lessThenEndTime = this.endTime >= screening.getWhenScreened();
-
-    return matchedWeek && moreThenStartTime && lessThenEndTime;
-  }
-
-  private isSatisfiedBySequence(screening: Screening): boolean {
-    return this.sequence === screening.getSequence();
-  }
+export interface DiscountCondition {
+  isSatisfiedBy(screening: Screening): boolean;
 }
 
-class PeriodCondition {
+class PeriodCondition implements DiscountCondition {
   private readonly dayOfWeek: DayOfWeek;
   private readonly startTime: Date;
   private readonly endTime: Date;
@@ -48,7 +16,7 @@ class PeriodCondition {
     this.endTime = endTime;
   }
 
-  private isSatisfiedBy(screening: Screening): boolean {
+  public isSatisfiedBy(screening: Screening): boolean {
     const matchedWeek = screening.getWhenScreened().getDay() === this.dayOfWeek;
     const moreThenStartTime = screening.getWhenScreened() >= this.startTime;
     const lessThenEndTime = this.endTime >= screening.getWhenScreened();
@@ -57,14 +25,14 @@ class PeriodCondition {
   }
 }
 
-class SequenceCondition {
+class SequenceCondition implements DiscountCondition {
   private readonly sequence: number;
 
   constructor(sequence: number) {
     this.sequence = sequence;
   }
 
-  private isSatisfiedBy(screening: Screening): boolean {
+  public isSatisfiedBy(screening: Screening): boolean {
     return this.sequence === screening.getSequence();
   }
 }
