f108294e18afc75fd48e48bc80bfb73d441a367f
kjkandrea <kjkandrea@gmail.com>
2022-12-31 12:51:47 +0900

    feat: Movie 책임을 수행하는데에 필요한 인스턴스 변수 추가

--- src/screening-reservation/responsibility-driven/Movie.ts
+++ src/screening-reservation/responsibility-driven/Movie.ts
@@ -1,6 +1,19 @@
 import {Screening} from './Screening';
 import {Money} from '../global/Money';
+import {Duration} from '../../global/datetime/Duration';
+import {DiscountCondition} from './DiscountCondition';
+
+type MovieType = 'NONE_DISCOUNT' | 'AMOUNT_DISCOUNT' | 'PERCENT_DISCOUNT';
 
 export abstract class Movie {
+  private title: string;
+  private runningTime: Duration;
+  private readonly fee: Money;
+  private readonly discountConditions: DiscountCondition[] = [];
+
+  private readonly movieType: MovieType;
+  private readonly discountAmount: Money;
+  private readonly discountPercent: number;
+
   public abstract calculateMovieFee(screening: Screening): Money;
 }
