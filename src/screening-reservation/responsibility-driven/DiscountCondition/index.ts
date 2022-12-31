df0b90d8ab01a48ab0f57c0497d30e9d1a58a878
kjkandrea <kjkandrea@gmail.com>
2022-12-31 13:09:06 +0900

    feat: DiscountCondition 책임을 수행하는데에 필요한 인스턴스 변수 추가
    냄새가 난다~~~

--- src/screening-reservation/responsibility-driven/DiscountCondition/index.ts
+++ src/screening-reservation/responsibility-driven/DiscountCondition/index.ts
@@ -1,5 +1,15 @@
 import {Screening} from '../Screening';
+import {DayOfWeek} from '../../../global/datetime/dayOfWeek';
+
+type DiscountConditionType = 'SEQUENCE' | 'PERIOD';
 
 export abstract class DiscountCondition {
+  private readonly type: DiscountConditionType;
+
+  private readonly sequence: number;
+  private readonly dayOfWeek: DayOfWeek;
+  private readonly startTime: Date;
+  private readonly endTime: Date;
+
   public abstract isSatisfiedBy(screening: Screening): boolean;
 }
