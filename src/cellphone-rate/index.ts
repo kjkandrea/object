efdc47f3308926087d9586cab3bf33903650ef74
kjkandrea <kjkandrea@gmail.com>
2023-01-27 11:39:13 +0900

    feat: Call 추가

--- src/cellphone-rate/index.ts
+++ src/cellphone-rate/index.ts
@@ -1,1 +1,19 @@
-console.log('cellphone-rate');
+class Call {
+  private from: Date;
+  private to: Date;
+
+  constructor(from: Date, to: Date) {
+    this.from = from;
+    this.to = to;
+  }
+
+  public getDuration(): number {
+    return this.to.getTime() - this.from.getTime();
+  }
+}
+
+const to = new Date();
+const from = new Date(to.getTime() - 1000 * 60 * 5);
+
+const call = new Call(from, to);
+console.log(`통화 시간 : ${call.getDuration() / 1000}초`);
