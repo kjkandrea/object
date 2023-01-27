32c381a97a08919619ea455a5fc0c39e815dedfd
kjkandrea <kjkandrea@gmail.com>
2023-01-27 11:26:43 +0900

    feat: cellphone rate 프로젝트 생성

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 // import './little-theater/to-be';
 
-console.log('--- role-driven ---');
-import './screening-reservation/role-driven';
-console.log('--- data-driven ---');
-import './screening-reservation/data-driven';
+// console.log('--- role-driven ---');
+// import './screening-reservation/role-driven';
+// console.log('--- data-driven ---');
+// import './screening-reservation/data-driven';
+
+import './cellphone-rate';
