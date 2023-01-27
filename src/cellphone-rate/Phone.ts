bb7ff270ee36a73f7f938007ce515837e9dde2c2
kjkandrea <kjkandrea@gmail.com>
2023-01-27 17:07:45 +0900

    feat: 세금 정책 제거

--- src/cellphone-rate/Phone.ts
+++ src/cellphone-rate/Phone.ts
@@ -4,24 +4,17 @@ import Call from 'cellphone-rate/Call';
 
 // 통화 목록을 계산하는 방법이 바뀔 경우만 변경된다.
 abstract class Phone {
-  private readonly taxRate: number;
   private calls: Call[] = [];
-
-  protected constructor(taxRate: number) {
-    this.taxRate = taxRate;
-  }
 
   public call(call: Call) {
     this.calls.push(call);
   }
 
   public calculateFee(): Money {
-    const fee = this.calls.reduce((totalMoney, call) => {
+    return this.calls.reduce((totalMoney, call) => {
       totalMoney = totalMoney.plus(this.calculateCallFee(call));
       return totalMoney;
     }, Money.ZERO);
-
-    return fee.plus(fee.times(this.taxRate));
   }
 
   protected abstract calculateCallFee(call: Call): Money;
@@ -32,8 +25,8 @@ export class RegularPhone extends Phone {
   private readonly amount: Money;
   protected readonly seconds: Seconds;
 
-  constructor(amount: Money, seconds: Seconds, taxRate: number) {
-    super(taxRate);
+  constructor(amount: Money, seconds: Seconds) {
+    super();
     this.amount = amount;
     this.seconds = seconds;
   }
@@ -51,13 +44,8 @@ export class NightDiscountPhone extends Phone {
   private readonly regularAmount: Money;
   private readonly seconds: Seconds;
 
-  constructor(
-    nightlyAmount: Money,
-    regularAmount: Money,
-    seconds: Seconds,
-    taxRate: number
-  ) {
-    super(taxRate);
+  constructor(nightlyAmount: Money, regularAmount: Money, seconds: Seconds) {
+    super();
     this.nightlyAmount = nightlyAmount;
     this.regularAmount = regularAmount;
     this.seconds = seconds;
