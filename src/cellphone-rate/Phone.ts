cbaaaebd1101bd37e083c10b9c3e9219a3bb650c
kjkandrea <kjkandrea@gmail.com>
2023-01-27 12:41:10 +0900

    feat: calls 요금을 계산하는 Phone 생성

---
+++ src/cellphone-rate/Phone.ts
@@ -0,0 +1,39 @@
+import {Money} from 'global/Money';
+import {Seconds} from 'cellphone-rate/types';
+import Call from 'cellphone-rate/Call';
+
+export default class Phone {
+  private amount: Money;
+  private seconds: Seconds;
+  private calls: Call[] = [];
+
+  constructor(amount: Money, seconds: Seconds) {
+    this.amount = amount;
+    this.seconds = seconds;
+  }
+
+  public call(call: Call) {
+    this.calls.push(call);
+  }
+
+  public getCalls(): Call[] {
+    return this.calls;
+  }
+
+  public getAmount(): Money {
+    return this.amount;
+  }
+
+  public getSeconds(): Seconds {
+    return this.seconds;
+  }
+
+  public calculrateFee(): Money {
+    return this.calls.reduce((totalMoney, call) => {
+      totalMoney = totalMoney.plus(
+        this.amount.times(call.getDurationSeconds() / this.seconds)
+      );
+      return totalMoney;
+    }, Money.ZERO);
+  }
+}
