46776f856d522fa7ce9797f37e6dba1176e42204
kjkandrea <kjkandrea@gmail.com>
2023-01-31 09:54:37 +0900

    feat: Professor 인자 GradeLecture 로 변경

--- src/grade-calculation/index.ts
+++ src/grade-calculation/index.ts
@@ -24,7 +24,18 @@ console.log(gradeLecture.evaluate());
 
 const professor = new Professor(
   '다익스트라',
-  new Lecture('알고리즘', 70, [81, 95, 75, 50, 45])
+  new GradeLecture(
+    '알고리즘',
+    70,
+    [
+      new Grade('A', 100, 95),
+      new Grade('B', 94, 80),
+      new Grade('C', 79, 70),
+      new Grade('D', 69, 50),
+      new Grade('F', 49, 0),
+    ],
+    [81, 95, 75, 50, 45]
+  )
 );
 
 console.log(professor.compileStatistics());
