22e0ad3a2cf7b2970ed60e1b614036c1ffc107f6
kjkandrea <kjkandrea@gmail.com>
2023-02-01 11:10:43 +0900

    feat: DateTimeInterval 유틸 추가

---
+++ src/cellphone-rate/RatePolicy/utils/DateTimeInterval.ts
@@ -0,0 +1,47 @@
+export class DateTimeInterval {
+  private readonly from: Date;
+  private readonly to: Date;
+
+  public static of(from: Date, to: Date): DateTimeInterval {
+    return new DateTimeInterval(from, to);
+  }
+
+  public static toMidnight(from: Date): DateTimeInterval {
+    const to = from;
+    to.setHours(23, 59, 59, 999);
+
+    return new DateTimeInterval(from, to);
+  }
+
+  public static fromMidnight(to: Date): DateTimeInterval {
+    const from = to;
+    from.setHours(23, 59, 59, 999);
+
+    return new DateTimeInterval(from, to);
+  }
+
+  public static during(date: Date): DateTimeInterval {
+    const from = date;
+    from.setHours(0, 0);
+    const to = date;
+    to.setHours(23, 59, 59, 999);
+    return new DateTimeInterval(from, to);
+  }
+
+  constructor(from: Date, to: Date) {
+    this.from = from;
+    this.to = to;
+  }
+
+  public duration(): number {
+    return this.to.getTime() - this.from.getTime();
+  }
+
+  public getFrom(): Date {
+    return this.from;
+  }
+
+  public getTo(): Date {
+    return this.to;
+  }
+}
